339053b88e8f2d7ecfe00ae6bc1484b478091d1e
albertvidal <albert@axis-studios.com>
2017-11-28 19:02:04 +0100

    test

--- git-hook-devon/deploy.php
+++
@@ -1,8 +0,0 @@
-<?php
-$command = "cd /home/wirexmed/public_html/devon ; git pull --progress --no-rebase -v origin 2>&1";
-exec($command, $output, $status);
-echo htmlentities(implode("\n", $output)).'<br />';
-
-/*$command = "git clone -b master https://github.com/wirexmedia/devon.git /home/wirexmed/public_html/devon 2>&1";
-exec($command, $output, $status);
-echo htmlentities(implode("\n", $output)).'<br />';*/
