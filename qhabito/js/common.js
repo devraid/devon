1a52d9cc0aecc87cbb6fdfb25815df6950797691
Albert <albert@wirexmedia.com>
2017-12-29 13:03:15 +0100

    general

--- qhabito/js/common.js
+++ qhabito/js/common.js
@@ -9,8 +9,31 @@ var QHABITO = window.QHABITO || {};
 	function Common() {
 		QHABITO.common = {
 			target : $('div.main'),
+			getCookie: function(key) {
+				var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+				return keyValue ? keyValue[2] : null;
+			},
+			setCookie: function(key, value) {
+				var expires = new Date();
+				expires.setTime(expires.getTime() + (360 * 24 * 60 * 60 * 1000));
+				document.cookie = key + '=' + value + ';path=/;expires=' + expires.toUTCString();
+			},
 			init: function() {
-				console.log('hello');
+				// Cookies
+				var cookies = $('.cookies');
+				var qh_cookies = QHABITO.common.getCookie('qh_cookies');
+				if (!qh_cookies) {
+					cookies.addClass('visible');
+					$('.button', cookies).on('click', function() {
+						$(this).blur();
+						QHABITO.common.setCookie('qh_cookies', 'true');
+						cookies.remove();
+						return false;
+					});
+				} else {
+					cookies.remove();
+				}
+				
 				// It's always good to focus on things :)
 				$(window).focus();
 			}
